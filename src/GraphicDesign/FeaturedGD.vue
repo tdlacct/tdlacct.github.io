e672385f607d412c052a3fd716e0f74d924a7ee1
T <timothymailacct@gmail.com>
2020-10-19 07:18:41 -0500

    New digital art

--- src/GraphicDesign/FeaturedGD.vue
+++ src/GraphicDesign/FeaturedGD.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
 <div style="text-align: center; margin: auto;">
-  <h3>Featured Work</h3>
+  <h3>What's New?</h3>
   <p>Take a look at what has been recently added to the portfolio! Hover over an image to get a closer look</p>
 </div>
 <featured :images='fullarray'></featured>
